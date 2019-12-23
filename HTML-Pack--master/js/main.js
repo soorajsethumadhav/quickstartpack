13a7bb7439cf492ec13ee3a9d7fb92449204f5c0
soorajsethumadhav <38313682+soorajsethumadhav@users.noreply.github.com>
2019-12-23 16:05:52 +0530

    Add files via upload

---
+++ HTML-Pack--master/js/main.js
@@ -0,0 +1,105 @@
+(function ($) {
+"use strict";
+
+// meanmenu
+$('#mobile-menu').meanmenu({
+	meanMenuContainer: '.mobile-menu',
+	meanScreenWidth: "992"
+});
+
+jQuery(document).ready(function($){
+        $(".homepage-slides").owlCarousel({
+            items: 1,
+            nav: true,
+            dots: false,
+            autoplay: true,
+            loop: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            mouseDrag: false,
+            touchDrag: false,
+        });
+        
+        $(".homepage-slides").on("translate.owl.carousel", function(){
+            $(".single-slide-item h2, .single-slide-item p").removeClass("animated fadeInUp").css("opacity", "0");
+            $(".single-slide-item .slide-btn").removeClass("animated fadeInDown").css("opacity", "0");
+        });
+        
+        $(".homepage-slides").on("translated.owl.carousel", function(){
+            $(".single-slide-item h2, .single-slide-item p").addClass("animated fadeInUp").css("opacity", "1");
+            $(".single-slide-item .slide-btn").addClass("animated fadeInDown").css("opacity", "1");
+        });
+        
+
+
+
+// mainSlider
+function mainSlider() {
+	var BasicSlider = $('.slider-active');
+	BasicSlider.on('init', function (e, slick) {
+		var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+		doAnimations($firstAnimatingElements);
+	});
+	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+		doAnimations($animatingElements);
+	});
+	BasicSlider.slick({
+		autoplay: false,
+		autoplaySpeed: 10000,
+		dots: false,
+		fade: true,
+		arrows: false,
+		responsive: [
+			{ breakpoint: 767, settings: { dots: false, arrows: false } }
+		]
+	});
+
+	function doAnimations(elements) {
+		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+		elements.each(function () {
+			var $this = $(this);
+			var $animationDelay = $this.data('delay');
+			var $animationType = 'animated ' + $this.data('animation');
+			$this.css({
+				'animation-delay': $animationDelay,
+				'-webkit-animation-delay': $animationDelay
+			});
+			$this.addClass($animationType).one(animationEndEvents, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+}
+mainSlider();
+
+
+// owlCarousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+	items:1,
+	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    nav:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        767:{
+            items:3
+        },
+        992:{
+            items:5
+        }
+    }
+})
+
+
+
+
+
+// WOW active
+new WOW().init();
+
+
+})(jQuery);
